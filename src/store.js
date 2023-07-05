15879c392968af987aadbb735266efe7f7688663
PintoChoudhary <pintochoudhary62@gmail.com>
2023-07-05 21:07:32 +0530

    My 1st commit

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tasksSlice from "./reducer/tasksSlice";
+export const store = configureStore ({
+    reducer : {
+        tasks : tasksSlice,
+    }
+})
